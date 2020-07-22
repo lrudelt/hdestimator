801a8795230d272ff80cc9074d7d643a633d17b1
Lucas Rudelt <lucas@viboras.lmp.ds.mpg.de>
2020-07-22 16:04:53 +0200

    last cahnges

--- exe/emb_opt_analysis.py
+++ exe/emb_opt_analysis.py
@@ -31,23 +31,25 @@ if recording == 'Other':
     setting_file = '%s/%s_%s_settings.yaml' % (codedirectory, setting, area)
 if recording == 'Simulated':
     load_script = '%s/exe/load_data_Simulated.py')
-    setting_file = '%s/%s_Simulated_settings.yaml' % (codedirectory, setting)
+    setting_file='%s/%s_Simulated_settings.yaml' % (codedirectory, setting)
 
-program ='/home/lucas/anaconda2/envs/python3/bin/python -s'
-program = '/home/lucas/anaconda3/bin/python -s'
-script = '%s/estimate.py' % (codedirectory)
+program='/home/lucas/anaconda2/envs/python3/bin/python -s'
+program='/home/lucas/anaconda3/bin/python -s'
+script='%s/estimate.py' % (codedirectory)
 
 if recording == "Simulated":
-    command =  program + ' ' + load_script + ' ' + rec_length + ' | ' + \
-               program + ' /dev/stdin -t hist -p -s ' + setting_file + ' -l "{}-{}-{}"'.format(rec_length, setting, sample_index)
+    command=program + ' ' + load_script + ' ' + rec_length + ' | ' +
+               program + ' /dev/stdin -t hist -p -s ' + setting_file +
+                   ' -l "{}-{}-{}"'.format(rec_length, setting, sample_index)
 else:
-    command =  program + ' ' + load_script + ' ' + rec_length + ' | ' + \
-               program + ' /dev/stdin -t hist -p -s ' + setting_file + ' -l "{}-{}"'.format(rec_length, setting)
+    command=program + ' ' + load_script + ' ' + rec_length + ' | ' +
+               program + ' /dev/stdin -t hist -p -s ' + setting_file +
+                   ' -l "{}-{}"'.format(rec_length, setting)
 # Make sure that the threads are on a single core
 
-call(command, shell=True)
+call(command, shell = True)
 
 
-# TODO: Write out load_data_scripts (idea for Simulated: choose 10 evenly spaced consecutive samples from the whole recordin, but how to choose cross-validated? Only perform embedding optimization on the first half and evaluate on the full recording, or take the next recording in line)
+# TODO: Write out load_data_scripts (idea for Simulated: choose 10 evenly spaced consecutive samples from the whole recording, but how to choose cross-validated? Only perform embedding optimization on the first half and evaluate on the full recording, or take the next recording in line)
 # TODO: Write settings files
 # TODO: Check if your function call is correct, and test on example data set with reduced setting
